09978364c6963e6deaa10c72d1dc29dd05041ddd
Nickolay Laptev <nickolay.laptev@gmail.com>
2025-06-25 21:54:36 +0300

    Disable jekyll

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  assetPrefix: './',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
