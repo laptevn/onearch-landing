9f3cb247f8668744ddf7cfcdbdef7c5be0a84372
Nickolay Laptev <nickolay.laptev@gmail.com>
2025-06-25 14:50:36 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Benefits from "@/components/landing/benefits";
 import Founder from "@/components/landing/founder";
 import Faq from "@/components/landing/faq";
 import Cta from "@/components/landing/cta";
+import Footer from "@/components/landing/footer";
 
 export const dynamic = 'force-static';
 
